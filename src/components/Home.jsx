ad984af739c3b33b7e6e6497ffa8e8ae9b67d324
kake <kaisla.ketola@metropolia.fi>
2024-04-17 09:48:39 +0300

    Add postcss.config.js, .prettierrc, tailwind.config.js, src/components/UI/Button.jsx, and package.json

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,7 @@
+import {useState} from 'react';
 import MediaRow from './MediaRow';
+import SingleView from './SingleView';
+
 const mediaArray = [
   {
     media_id: 8,
@@ -37,6 +40,10 @@ const mediaArray = [
 ];
 
 const Home = () => {
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  console.log(selectedItem);
+
   return (
     <>
       <h2>My Media</h2>
@@ -49,14 +56,23 @@ const Home = () => {
             <th>Created</th>
             <th>Size</th>
             <th>Type</th>
+            <th>Actions</th>
           </tr>
         </thead>
         <tbody>
           {mediaArray.map((item) => (
-            <MediaRow key={item.media_id} item={item} />
+            <MediaRow
+              key={item.media_id}
+              item={item}
+              setSelectedItem={setSelectedItem}
+            />
           ))}
         </tbody>
       </table>
+      <SingleView
+        selectedItem={selectedItem}
+        setSelectedItem={setSelectedItem}
+      />
     </>
   );
 };
